ebcd28c7b52a502079bba3adb27e33e83157c02a
Andy <xcessivepupil2@gmail.com>
2019-11-26 22:46:34 -0800

    First commit with completed stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,27 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+    var result = [];
+    var resultObj = [];
+    if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
+        return '' + obj; 
+    } else if (typeof obj === 'string')	{
+        return '\"' + obj + "\"";
+    } else if (typeof obj === 'function' || typeof obj === undefined) {
+        return '';
+    } else if (Array.isArray(obj)) {
+	    for (i=0; i<obj.length;i++) {
+            result.push(stringifyJSON(obj[i]));
+	    }
+	    return '[' + result + ']';
+    } else if(typeof obj === "object") {
+        for (var key in obj) {
+            if (typeof obj[key] === 'function' || obj[key] === undefined) {
+                break;
+            } else {
+                resultObj.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+            }
+	    }
+	    return "{" + resultObj + "}";
+    }
 };
